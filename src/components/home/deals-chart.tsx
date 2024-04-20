ee4f5143bf3250e79f330fb2fd090f0b3f6da5f9
eimanes <eimansalleh.5@gmail.com>
2024-04-20 23:37:45 +0800

    update

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,14 +1,52 @@
+import { DollarOutlined } from '@ant-design/icons';
 import { Card } from 'antd'
+import { Text } from '../text'
 import React from 'react'
+import { Area, AreaConfig } from '@ant-design/plots';
+import { DASHBOARD_DEALS_CHART_QUERY } from '../../graphql/queries';
+import { useList } from '@refinedev/core';
+import { mapDealsData } from '../../utilities/helpers';
+import { GetFieldsFromList } from '@refinedev/nestjs-query';
+import { DashboardDealsChartQuery } from '../../graphql/types'
 
 const DealsChart = () => {
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: 'dealStages',
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY
+    }
+  });
+
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data])
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: 'timeText',
+    yField: 'value'
+  };
+
   return (
     <Card
       style={{ height: '100%' }}
       headStyle={{ padding: '8px 16px' }}
-      bodyStyle={{ padding: '0 1rem' }}
+      bodyStyle={{ padding: '24px 24px 0 24px' }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px'
+          }}>
+          <DollarOutlined />
+          <Text size='sm' style={{ marginLeft: '0.5rem' }}>
+            Deals
+          </Text>
+        </div>
+      }
     >
-      DealsChart
+      <Area {...config} height={325} />
     </Card>
   )
 }
